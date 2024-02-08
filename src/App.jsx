0fdeaf664647b8bc00ac7db67c15501a23e96a39
mit0145 <haritrami014@gmail.com>
2024-02-08 22:31:25 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "./components/Hero";
+import Stats from "./components/Stats";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <div>
+      <Hero />
+      <Stats />
+      <Footer />
+    </div>
+  );
+}
